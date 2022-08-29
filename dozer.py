e0bbbecc4741f78105369554620005df665a245c
Nephron <na.semertzidis@gmail.com>
2022-08-29 22:52:31 +1000

    replace ml with thresholding

--- dozer.py
+++ dozer.py
@@ -5,6 +5,7 @@ import stimulations
 
 def main():
 
+    threshold_summation = 0
     cooldown = 0
     sleepy_class = pickle.load(open('sleepy_class.sav', 'rb'))
     # initialise BCI parameters
@@ -15,9 +16,11 @@ def main():
     # MAIN LOOP #
     while True:
         
-        time.sleep(5) 
+        time.sleep(10) 
         bci.update_data(bci.eeg_channels_dozer) # prompts the BCI to pull any new data from the data buffer
-        sleepiness = bci.get_sleepieness(sleepy_class)
+        # sleepiness = bci.get_sleepieness(sleepy_class)
+        alpha_theta = bci.get_alpha_theta_ratio(bci.data, bci.eeg_channels_dozer)
+        mean_alpha_theta = alpha_theta.mean(axis=1)
 
         print('Cooldown: %s' % str(cooldown))
 
@@ -25,14 +28,19 @@ def main():
         if cooldown > 0:
             cooldown -= 5
 
-        if sleepiness == 0:
-            print("not sleepy")
+        if mean_alpha_theta < 1.0:
+            threshold_summation += 1
+        else:
+            threshold_summation = 0
 
-        if sleepiness == 1:
-            print("sleepy")
+        # if sleepiness == 0:
+        #     print("not sleepy")
+
+        # if sleepiness == 1:
+        #     print("sleepy")
 
         # if user is sleepy and cooldown is not active, begin stimulation and activate cooldown
-        if sleepiness == 1 and cooldown == 0:
+        if threshold_summation == 3 and cooldown == 0:
             stimulations.stimulate_all()
             cooldown = 28800 #28800 seconds = 480 minutes = 8 hours. Lets the user sleep
 
